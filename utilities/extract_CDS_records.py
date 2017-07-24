fa4f0a115fae7416f7656ca96be275641112343a
Brian Haas <brianjohnhaas@gmail.com>
2017-07-24 13:34:47 -0400

    renamed

--- utilities/extract_CDS_records.py
+++ utilities/extract_CDS_records.py
@@ -1,33 +1,24 @@
-#Create gff file of CDS predictions from Transdecoder, GeneMarkS-T, Prodigal
-from dependency import *
-from arguments import args
-path = os.getcwd()
+#!/usr/bin/env python
 
-#convert algorithm outputs to gff format
-algo = args.algorithm.lower()
-file_name = args.filename
-num = args.i
+import sys
 
-def op2gff():
-    if algo == 'transdecoder':
-        if num == 1:
-            f_pred = open(path+'/'+file_name+'.transdecoder.gff3',"r")
-        elif num == 2:
-            f_pred = open(path+'/'+file_name+'.transdecoder_dir/longest_orfs.gff3',"r")
-        elif num == 3:
-            f_pred = open(path+'/'+file_name+'.transdecoder.gff3-single-best',"r")
-        f_out = open(path+'/'+file_name+'_transdecoder_output.gff',"w")
-    elif algo == 'genemark':
-        f_pred = open(path+'/'+file_name+'.gff',"r")
-        f_out = open(path+'/'+file_name+'_genemark_output.gff',"w")
-    elif algo == 'prodigal':
-        f_pred = open(path+'/prodigal_'+file_name+'.gff',"r")
-        f_out = open(path+'/'+file_name+'_prodigal_output.gff',"w")
-    prediction = f_pred.readlines()
-    #write CDS prediction to an output file
-    for pred in prediction:
-        if '\tCDS\t' in pred:
-            f_out.write(pred)
-    f_pred.close()
-    f_out.close()
+usage = "usage: {} input.gff > output.cds.gff\n\n".format(sys.argv[0])
+if len(sys.argv) < 2:
+    sys.stderr.write(usage)
+    sys.exit(1)
+
+input_gff = sys.argv[1]
+
+f_pred = open(input_gff)
+#write CDS prediction to an output file
+predictions = f_pred.readlines()
+for pred in predictions:
+    if '\tCDS\t' in pred:
+        sys.stdout.write(pred)
+
+
+sys.exit(0)
+
+
+
 

05edd53b081fc46e022b19a6e1c30cdd484a6f5a
Brian Haas <brianjohnhaas@gmail.com>
2017-07-26 11:24:24 -0400

    update

--- utilities/score_TP_FP_FN.py
+++ utilities/score_TP_FP_FN.py
@@ -37,13 +37,17 @@ for line in truth_lines:
     x = line.split("\t")
 
     transcript_id = x[0]
-    
-    truth_orf = { 'transcript_id' : transcript_id,
-                  'ref_start' : int(x[3]),
-                  'ref_end' : int(x[4]),
-                  'ref_orient' : x[6],
-                  'ref_length' : int(x[4]) - int(x[3]) + 1
-                  }
+
+    try:
+        truth_orf = { 'transcript_id' : transcript_id,
+                      'ref_start' : int(x[3]),
+                        'ref_end' : int(x[4]),
+                      'ref_orient' : x[6],
+                      'ref_length' : int(x[4]) - int(x[3]) + 1
+                      }
+    except:
+        sys.stderr.write("Error parsing coordinate info from line: {}\n".format(line))
+        continue
     
     truth_predictions[transcript_id] = truth_orf
 
@@ -51,47 +55,56 @@ for line in truth_lines:
 #store all predictions
 correctly_predicted_transcripts = set()
 for pred in prediction:
-     line = pred.strip().split('\t')
-     transcript_id = line[0].split('|')[0]
+    
+    line = pred.strip().split('\t')
+    transcript_id = line[0].split('|')[0]
+    
+    ref_st = -1
+    ref_end = -1
+    ref_orient = '?'
+    ref_length = -1
+    
+    if transcript_id in truth_predictions:
+        truth_struct = truth_predictions[transcript_id]
+        ref_st = truth_struct['ref_start']
+        ref_end = truth_struct['ref_end']
+        ref_orient = truth_struct['ref_orient']
+        ref_length = truth_struct['ref_length']
+    else:
+        sys.stderr.write("error, missing {} in truth set\n".format(transcript_id))
 
-     ref_st = -1
-     ref_end = -1
-     ref_orient = '?'
-     ref_length = -1
+    pred_st = -1
+    pred_end = -1
 
-     if transcript_id in truth_predictions:
-         truth_struct = truth_predictions[transcript_id]
-         ref_st = truth_struct['ref_start']
-         ref_end = truth_struct['ref_end']
-         ref_orient = truth_struct['ref_orient']
-         ref_length = truth_struct['ref_length']
-     else:
-         sys.stderr.write("error, missing {} in truth set\n".format(transcript_id))
+    try:
+        pred_st = int(line[3])
+        pred_end = int(line[4])
+    except:
+        sys.stderr.write("Error, cannot parse coordinate info from line: {}\n".format(pred))
+        continue
+    
+    pred_orient = line[6]
+    pred_length = pred_end - pred_st + 1
      
-     pred_st = int(line[3])
-     pred_end = int(line[4])
-     pred_orient = line[6]
-     pred_length = pred_end - pred_st + 1
+    result = "\t".join([transcript_id,
+                        str(ref_st), str(ref_end), ref_orient, str(ref_length),
+                        str(pred_st), str(pred_end), pred_orient, str(pred_length)])
      
-     result = "\t".join([transcript_id,
-                         str(ref_st), str(ref_end), ref_orient, str(ref_length),
-                         str(pred_st), str(pred_end), pred_orient, str(pred_length)])
-     
-     if (ref_orient == pred_orient
-         and
-         abs(ref_end - pred_end) < 3
-         and
-         abs(ref_st - pred_st) < 3 ) :
+    if (ref_orient == pred_orient
+        and
+        abs(ref_end - pred_end) < 3
+        and
+        abs(ref_st - pred_st) < 3 ) :
          
-          three_five_match.append("\t".join(['TP', result, '3,5-prime']))
-          correctly_predicted_transcripts.add(transcript_id)
+        three_five_match.append("\t".join(['TP', result, '3,5-prime']))
+        correctly_predicted_transcripts.add(transcript_id)
 
-     elif ref_orient == pred_orient and abs(ref_end - pred_end) < 3:
-          three_match.append("\t".join(['TP', result, '3-prime']))
-          correctly_predicted_transcripts.add(transcript_id)
-
-     else:
-          false_pos.append("\t".join(['FP', result, '.']))
+    elif ref_orient == pred_orient and abs(ref_end - pred_end) < 3:
+        three_match.append("\t".join(['TP', result, '3-prime']))
+        correctly_predicted_transcripts.add(transcript_id)
+        
+    else:
+        false_pos.append("\t".join(['FP', result, '.']))
 
 
 #store missing predictions

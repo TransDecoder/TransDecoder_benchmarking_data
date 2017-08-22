7d223f6852d14a6c5467c74b9300ed9e5da78648
Brian Haas <brianjohnhaas@gmail.com>
2017-08-22 14:06:23 -0400

    add

---
+++ complete/a_thaliana/__custom_filters/run_analysis.sh
@@ -0,0 +1,8 @@
+~/GITHUB/TransDecoder/util/misc/select_TD_orfs.py  longest_orfs.cds longest_orfs.cds.scores
+
+~/GITHUB/TransDecoder_benchmarking_data/utilities/run_analysis_pipeline.py  data.json analysis_dir/
+
+~/GITHUB/TransDecoder/util/misc/get_FP_FN_scores.py  longest_orfs.cds.scores longest_orfs.cds analysis_dir/def_custom_single.cds.gff.scored | sort -k12,12gr > custom.FP_FNs.dat
+
+
+ 

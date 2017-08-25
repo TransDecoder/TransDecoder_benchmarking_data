cfdfe37bc6c7586201574b3a5936279dae4b9fd6
Brian Haas <brianjohnhaas@gmail.com>
2017-08-25 08:39:54 -0400

    add

--- complete/a_thaliana/__custom_filters/run_analysis.sh
+++ complete/a_thaliana/__custom_filters/run_analysis.sh
@@ -28,6 +28,15 @@ set -ev
 ~/GITHUB/TransDecoder_benchmarking_data/utilities/run_analysis_pipeline.py data.rpart.all.json all
 
 
+## td alg vary:
+
+../../__rpart/vary_TD_selection_rules.py  --long_orfs_cds longest_orfs.cds --long_orfs_scores longest_orfs.cds.scores
+
+~/GITHUB/TransDecoder_benchmarking_data/utilities/run_analysis_pipeline.py data.TDvary.json analysis_TDvary
+
+~/GITHUB/TransDecoder_benchmarking_data/utilities/run_analysis_pipeline.py data.TDvary.single.json analysis_TDvary_single
+
+
 
 
  

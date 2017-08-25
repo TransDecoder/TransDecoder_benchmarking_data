146cc884cc8701c9e0167f5c227a33f9da04bc20
Brian Haas <brianjohnhaas@gmail.com>
2017-08-25 12:22:57 -0400

    updated fragment generator

--- utilities/create_fragments.py
+++ utilities/create_fragments.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #creates random fragments from a list of transcripts
 import numpy as np
 import os,sys,argparse
@@ -19,33 +21,41 @@ dist = np.random.normal(mu, sigma, len(lines))
 all_frag_trans = {}
 for i in range(len(lines)):
     if lines[i].startswith('>'):
-         chop = int(len(lines[i+1])*dist[i])
-         frag = [lines[i+1].strip()[:chop],lines[i+1].strip()[chop:]]
-         randomindex = random.randint(0,1)
-         length = len(frag[randomindex])
-         f = frag[randomindex]
-         cds_end = int(lines[i].split('|')[1].split(':')[1].split('-')[1])
-         cds_st = int(lines[i].split('|')[1].split(':')[1].split('-')[0])
-         if chop>= cds_st and chop<= cds_end:
-             if randomindex == 0:
-                cds_end = chop
-             elif randomindex == 1:
-                cds_st = 1
-                cds_end = cds_end-chop
-         else:
-             if chop > cds_end:
-                 f = lines[i+1].strip()[:chop]
-                 cds_st = cds_st
-                 cds_end = cds_end-1
-             elif chop < cds_st:
-                 f = lines[i+1].strip()[chop:]
-                 cds_st = cds_st-chop+1
-                 cds_end = cds_end-chop+1-1
-         if cds_end<0:    
-              print cds_end, cds_end,chop, lines[i]
-         line = lines[i].split('|')
-         key = line[0]+'|CDS:'+str(cds_st)+'-'+str(cds_end)+'|length:'+str(len(frag[randomindex]))+'|'
-         all_frag_trans[key] = f
+
+        # get orf coordinates
+        seq_header = lines[i]  # example: >AT3G25110-lcl_NC_003074_8_mrna_NM_113415_4_30732|CDS:1-559|length:809|
+
+        cds_end = int(seq_header.split('|')[1].split(':')[1].split('-')[1])
+        cds_st = int(seq_header.split('|')[1].split(':')[1].split('-')[0])
+
+        # select a breakpoint at a random position within the transcript length
+        seq = lines[i+1].rstrip()
+        chop_ratio = random.choice(dist)
+        chop_pos = int( (cds_end - cds_st) * chop_ratio) + cds_st
+        
+        # split the sequence at the breakpoint
+        frags = [seq[:chop_pos], seq[chop_pos:]]
+
+        # take the shorter one
+        f = frags[0]
+        adj_cds_st = cds_st
+        adj_cds_end = chop_pos
+
+        midpt = (cds_st + cds_end) / 2
+        
+        if chop_pos > midpt:
+            f = frags[1]
+            adj_cds_st = 1
+            adj_cds_end = cds_end-chop_pos
+        
+        frag_length = len(f)
+        
+        
+        line = lines[i].split('|')
+        key = line[0]+'|CDS:'+str(adj_cds_st)+'-'+str(adj_cds_end)+'|length:'+str(frag_length)+'|'
+        all_frag_trans[key] = f
+
+
 
 for key in all_frag_trans.keys():
     fw.write(key+'\n')
